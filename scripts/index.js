ac03e6153912b250dbd1fb2c9f565d45b80f3876
Neil802 <deven802@gmail.com>
2025-02-07 19:34:10 -0500

    Project 4 stage 3
    added open and close for modal

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,14 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector(".profile__button-edit");
+const profileEditModal = document.querySelector(".modal");
+const modalCloseButton = document.querySelector(".modal__button-close");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+modalCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
